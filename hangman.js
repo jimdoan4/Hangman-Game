d587d33c7c13ae13986c3709de014da7eea36e65
Jim Doan <jimdoan4@gmail.com>
2019-02-15 17:02:52 -0500

    creating underscore to the page

--- hangman.js
+++ hangman.js
@@ -2,11 +2,38 @@
 // Setting all the variables for what I want each function to do
 // Remember to Console.log() to check if each function/method works!
 
-var words = ['SERENA WILLIAMS', 30, 'TENNIS'];
-var livesLeft = 10;
-var lives = [];
+var words = ["SERENA WILLIAMS", "ORANGE", "TENNIS"];
+var livesRemaining = 10;
+var life = [];
 var underScores = [];
-var alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var alphabets = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
 var randomWords;
 var randomLetters;
 
@@ -14,5 +41,15 @@ var randomLetters;
 
 function start() {
   randomWords = words[Math.floor(Math.random() * words.length)];
-  console.log(start)
-}
+  // console.log(randomWords);
+  // Make underscores for each words
+  for (var i = 0; i < randomWords.length; i++) {
+    // console.log(i);
+    underScores.push("_"); }
+    // console.log(underScores);
+    document.querySelector('under').textContent = underScores;
+   }
+  
+
+// Every time you call start a game its going to give you a random word
+start();
