f5e2dc9d941e55f19c40528472cda98e152445ea
Jim Doan <jimdoan4@gmail.com>
2019-02-17 13:22:56 -0500

    creating more pseudocodes to start on creating functionality for each buttons etc

--- hangman.js
+++ hangman.js
@@ -1,6 +1,17 @@
 // Global Variables
 // Setting all the variables for what I want each function to do
 // Remember to Console.log() to check if each function/method works!
+// Create an array of words
+// Choose word randomly
+// Create underscores based on the length of the word
+// Track users guesses
+// Check if user guess is right
+// If right, push to the "right" array, otherwise, push to "wrong" array
+// create start button when game does not start unless user clicks on the start button to initaite game
+// Create clear button to clear the score and reset the game back to 10 lives and 0 points
+// Create letter buttons so everytime a user click on a letter button the letter will appear on the underscore, If wrong letter, user loses point by -1
+// Create hangman so if user clicks on correct letter, a body part will appear on the hangman
+// cCreate an alert if user gets all letters correct, an alert will advise "You Won!", if user is out of lives, an alert will advise "Sorry, try again!"
 
 var words = ["SERENA WILLIAMS", "ORANGE", "TENNIS"];
 var livesRemaining = 10;
