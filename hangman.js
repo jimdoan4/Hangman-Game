4c60ad8206ca5687eb872ffa1dfb8385e83c6bd4
Jim Doan <jimdoan4@gmail.com>
2019-02-17 14:26:35 -0500

    formatting the underscores and pushing correct words into the underscores

--- hangman.js
+++ hangman.js
@@ -8,29 +8,44 @@ let randomWords = words[Math.floor(Math.random() * words.length)];
 let random = words[randomWords];
 let correctWords = [];
 let wrongWords = [];
+// Check if user guess is right
+let livesRemaining = 10;
+let life = [];
+let randomLetters;
 // Create underscores based on the length of the word
 let underScore = [];
+
+// DOM Manipulations
+let underLines = document.querySelector('#text-here');
+
+
 // Track users guesses
 document.addEventListener('keypress', (event) => {
-  let keycode = event.keyCode;
-  let keyButtons = String.fromCharCode(keycode);
+  let keys = event.keyCode;
+  let keyButtons = String.fromCharCode(keys);
   // console.log(keyButtons)
   // If user chooses correct letter
-  if (keycode.indexOf(keycode) > -1) {
-    console.log(true);
+  if (words.indexOf(keys) > -1) {
+    // console.log(true);
+    correctWords.push(keys);
+    // replace underscore with the right letter 
+    underScore[words.indexOf(keys)] = keys;
+    // Checks to see if user word matches guesses
+    if (underScore.join('') === words) {
+      alert('You Win!');
+    }
+  } else {
+    wrongWords.push(keys);
   }
 });
 
-// Check if user guess is right
-let livesRemaining = 10;
 // If right, push to the "right" array, otherwise, push to "wrong" array
 // create start button when game does not start unless user clicks on the start button to initaite game
 // Create clear button to clear the score and reset the game back to 10 lives and 0 points
 // Create letter buttons so everytime a user click on a letter button the letter will appear on the underscore, If wrong letter, user loses point by -1
 // Create hangman so if user clicks on correct letter, a body part will appear on the hangman
 // Create an alert if user gets all letters correct, an alert will advise "You Won!", if user is out of lives, an alert will advise "Sorry, try again!"
-let life = [];
-let randomLetters;
+
 
 //Setting the Functions/Methods to make each action work!
 
@@ -41,6 +56,7 @@ function start() {
   for (var i = 0; i < randomWords.length; i++) 
   {
     underScore.push('_'); 
+    underLines[0].textContent = underScore.join(' ');
   }
     // console.log(underScore);
     document.querySelector('#text-here').textContent = underScore.join(' ');
