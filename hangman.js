3190e23539d4520da18a870e90f1f20849edfc0e
Jim Doan <jimdoan4@gmail.com>
2019-02-17 20:34:10 -0500

    made adjustment to underscore

--- hangman.js
+++ hangman.js
@@ -1,74 +1,135 @@
 // Global Variables
 // Setting all the variables for what I want each function to do
 // Remember to Console.log() to check if each function/method works!
-// Create an array of words
-let words = ["serena williams", "roder federer", "rafael nadal"];
-// Choose word randomly
-let randomWords = words[Math.floor(Math.random() * words.length)];
-let random = words[randomWords];
-let correctWords = [];
-let wrongWords = [];
-// Check if user guess is right
-let livesRemaining = 10;
-let life = [];
-let randomLetters;
-// Create underscores based on the length of the word
-let underScore = [];
-
-// DOM Manipulations
-let underLines = document.querySelector('#text-here');
-
-
-// Track users guesses
-document.addEventListener('keypress', (event) => {
-  let keys = event.keyCode;
-  let keyButtons = String.fromCharCode(keys);
-  // console.log(keyButtons)
-  // If user chooses correct letter
-  if (words.indexOf(keys) > -1) {
-    // console.log(true);
-    correctWords.push(keys);
-    // replace underscore with the right letter 
-    underScore[words.indexOf(keys)] = keys;
-    // Checks to see if user word matches guesses
-    if (underScore.join('') === words) {
-      alert('You Win!');
-    }
-  } else {
-    wrongWords.push(keys);
-  }
-});
-
 // If right, push to the "right" array, otherwise, push to "wrong" array
+// Create an array of words
 // create start button when game does not start unless user clicks on the start button to initaite game
 // Create clear button to clear the score and reset the game back to 10 lives and 0 points
 // Create letter buttons so everytime a user click on a letter button the letter will appear on the underscore, If wrong letter, user loses point by -1
 // Create hangman so if user clicks on correct letter, a body part will appear on the hangman
 // Create an alert if user gets all letters correct, an alert will advise "You Won!", if user is out of lives, an alert will advise "Sorry, try again!"
 
+var words = ["serena williams", "roger federer", "rafael nadal"];
+var alphabets = [
+"A",
+"B",
+"C",
+"D",
+"E",
+"F",
+"G",
+"H",
+"I",
+"J",
+"K",
+"L",
+"M",
+"N",
+"O",
+"P",
+"Q",
+"R",
+"S",
+"T",
+"U",
+"V",
+"W",
+"X",
+"Y",
+"Z"
+];
 
-//Setting the Functions/Methods to make each action work!
+// Choose word randomly
+var correctWords = [];
+var wrongWords = [];
+// Check if user guess is right
+var livesRemaining = 6;
+var life = [];
+var randomWords;
+var randomLetters;
+// Create underscores based on the length of the word
+var underScore = [];
 
+// LivesRemaining = 10;
+// DOM Manipulations
+
+// Underscores for each length of words
 function start() {
-  // randomWords = words[Math.floor(Math.random() * words.length)];
+  randomWords = words[Math.floor(Math.random() * words.length)];
   // console.log(randomWords);
   // Make underscores for each words
   for (var i = 0; i < randomWords.length; i++) 
   {
     underScore.push('_'); 
-    underLines[0].textContent = underScore.join(' ');
   }
     // console.log(underScore);
     document.querySelector('#text-here').textContent = underScore.join(' ');
-    life = [];
-    livesRemaining = 10;
-    document.querySelector('#lives').textContent = livesRemaining;
-  }  
-  // start();
+
+    // life = [];
+    // livesRemaining = 6;
+    // document.querySelector('#lives').textContent = livesRemaining;
+  }
+
+  start();
 
 
 
-// Every time you call start a game its going to give you a random word
-// start();
+
+
+
+
+
+
+
+
+
+
+//   alphabets.forEach(function(el) {
+//     var button = document.createElement('button')
+// //   button.className = "hello";
+// button.innerHTML = el;
+// var body = document.getElementById('buttons');
+// document.body.appendChild(button);
+// });
+
+//   document.getElementByTagName('main')[0].setAttribute("class", "hello");
+
+// button.addEventListener ("click", function() {
+// alert('hi');
+// });
+
+
+
+// // Track users guesses
+// document.addEventListener('keypress', (event) => {
+//   let keys = event.keyCode;
+//   let keyButtons = String.fromCharCode(keys);
+//   // console.log(keyButtons)
+//   // If user chooses correct letter
+//   if (words.indexOf(keys) > -1) {
+//     // console.log(true);
+//     correctWords.push(keys);
+//     // replace underscore with the right letter 
+//     underScore[words.indexOf(keys)] = keys;
+//     underLines[0].textContent = underScore.join(' ');
+//     // Checks to see if user word matches guesses
+//     if (underScore.join('') === randomWords) {
+//       alert('You Win!');
+//     }
+//   } else {
+//     wrongWords.push(keys);
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
 
 
