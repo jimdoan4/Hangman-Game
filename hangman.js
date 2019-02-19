98f6d14ec6ceb346ad6f3297bc0bb44595f78908
Jim Doan <jimdoan4@gmail.com>
2019-02-19 00:26:12 -0500

    formatted css

--- hangman.js
+++ hangman.js
@@ -11,35 +11,35 @@
 
 var words = ["apple", "blueberry", "strawberry", "banana", "orange"];
 // This will randomize the words
-var randomWords = "";
+var randomWords;
 // Deciding if to use this and use click method. Make it look like buttons
 var alphabets = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
 ];
 
 // Number of guesses left = 6;
@@ -84,23 +84,24 @@ function alpha() {
 }
 alpha();
 
+// Add onclick function for buttons
 document.addEventListener("click", buttonClicked);
 function buttonClicked() {
   var clickedElement = event.target;
   clickedElement.classList.add("buttonDisabled");
-  var chosenLetter = event.target.textContent;
+  var chosenLetter = event.target.innerHTML;
   console.log("var text " + chosenLetter);
   var guessesRemaining = document.getElementById("livesRemaining").innerHTML;
   console.log(randomWords);
   if (guessesRemaining > 0) {
     var letterElement = new Array();
     for (var i = 0; i < randomWords.length; i++) {
-      if (randomWords[i] == chosenLetter.toLowerCase()) {
+      if (randomWords[i] === chosenLetter.toLowerCase()) {
         console.log("found letter " + randomWords[i]);
         letterElement.push(i);
       }
     }
-    console.log("letter element length " + letterElement.length);
+    // console.log("letter element length " + letterElement.length);
     if (letterElement.length < 1) {
       guessesRemaining -= 1;
       document.getElementById("livesRemaining").innerHTML = guessesRemaining;
@@ -112,12 +113,12 @@ function buttonClicked() {
           }
         }
       }
-      document.getElementById("wordGuesses").textContent = blanks.join(" ");
+      document.getElementById("wordGuesses").innerHTML = blanks.join(" ");
     }
   }
 }
 
-// Underscores for each length of words
+// Create underscores for each words accordingly
 function underline() {
   randomWords = words[Math.floor(Math.random() * words.length)];
   blanks = [];
@@ -125,105 +126,28 @@ function underline() {
     blanks.push("_");
   }
   // console.log(underScore);
-  document.getElementById("wordGuesses").textContent = blanks.join(" ");
+  document.getElementById("wordGuesses").innerHTML = blanks.join(" ");
 }
 underline();
 
+// Function to start the game
 function startGame() {
   randomWords = words[Math.floor(Math.random() * words.length)];
   underline();
+  document.getElementById("livesRemaining").innerHTML = livesRemaining;
 }
+startGame();
 
+// Function to reset the game
 function clearGame() {
   randomWords = words[Math.floor(Math.random() * words.length)];
   underline();
-  document.getElementById("livesRemaining").innerHTML = 6;
+  document.getElementById("livesRemaining").innerHTML = livesRemaining;
 }
+clearGame();
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Disable buttons initially
-// function disableAlpha() {
-//   document.querySelector('.buttons'),disabled = true;
+// // Categories
+// function categories() {
 // }
-
-// disableAlpha();
-
-// function userGuesses() {
-// guess = 0;
-// for (i = 0; i < words.length; i++);
-//   if (words[i] === inboxGuesses) {
-//     guess = guess + 1;
-//     underScore[i] = letters;
-//   }
-// }
-
-// userGuesses();
-
-// function alphaButtons() {
-//   alphaNum = document.querySelector('.hello');
-//   alpha = document.createElement('ul');
-//   for (var i = 0; i < alphabets.length; i++) {
-//     alpha.id = 'alphabets';
-//     lines = document.createElement('li');
-//     lines.id = 'alpha';
-//     lines.textContent = alphabets[i];
-//     check();
-//     alphaNum.appendChild(alpha);
-//     alpha.appendChild(lines);
-//   }
-// }
-// button.addEventListener('click', addItem);
-// function alphaButtons(e) {
-//   e.preventDefault();
-//   var num = document.getElementByClassName('hello');
-//   var divli = document.createElement('div');
-//   divli.className = 'text.here';
-//   divli.appendChild(document.createTextNode(alphabets));
-//   alphabets.appendChild(num);
-// }
-
-//   alphabets.forEach(function(el) {
-//     var button = document.createElement('button')
-// //   button.className = "hello";
-// button.innerHTML = el;
-// var body = document.getElementById('buttons');
-// document.body.appendChild(button);
-// });
-
-//   document.getElementByTagName('main')[0].setAttribute("class", "hello");
-
-// button.addEventListener ("click", function() {
-// alert('hi');
-// });
-
-// // Track users guesses
-// document.addEventListener('keypress', (event) => {
-//   let keys = event.keyCode;
-//   let keyButtons = String.fromCharCode(keys);
-//   // console.log(keyButtons)
-//   // If user chooses correct letter
-//   if (words.indexOf(keys) > -1) {
-//     // console.log(true);
-//     correctWords.push(keys);
-//     // replace underscore with the right letter
-//     underScore[words.indexOf(keys)] = keys;
-//     underLines[0].textContent = underScore.join(' ');
-//     // Checks to see if user word matches guesses
-//     if (underScore.join('') === randomWords) {
-//       alert('You Win!');
-//     }
-//   } else {
-//     wrongWords.push(keys);
-//   }
-// });
+    
+// , ["serena williams", "roger federer", "rafael nadal", "na li"], ["javascript", "css", "html", "python"]
