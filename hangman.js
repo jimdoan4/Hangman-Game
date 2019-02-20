25df59d2a81b9c83199cd4967ba9ee8e0a601b41
Jim Doan <35070374+jimdoan4@users.noreply.github.com>
2019-02-20 16:56:29 -0500

    error, should be js file not html. Readded codes back to this file.

--- hangman.js
+++ hangman.js
@@ -1,101 +1,160 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.cs"
-    />
-    <title>Hang Game</title>
-  </head>
-  <!-- 𝔾𝕦𝕖𝕤𝕤 𝕥𝕙𝕖 𝕒𝕦𝕥𝕠𝕞𝕠𝕓𝕚𝕝𝕖'𝕤 𝕓𝕣𝕒𝕟𝕕 𝕟𝕒𝕞𝕖 -->
-  <body>
-    <header>
-      <h1 id="hangman">🄷🄰🄽🄶🄼🄰🄽 🄶🄰🄼🄴</h1>
-      <p>Find your CITY, find your MATCH</p>
-    </header>
+// Global Variables
+// Setting all the variables for what I want each function to do
+// Remember to Console.log() to check if each function/method works!
+// If right, push to the "right" array, otherwise, push to "wrong" array
+// Create an array of words
+// create start button when game does not start unless user clicks on the start button to initiate game
+// Create clear button to clear the score and reset the game back to 10 lives and 0 points
+// Create letter buttons so every time a user click on a letter button the letter will appear on the underscore, If wrong letter, user loses point by -1
+// Create hangman so if user clicks on correct letter, a body part will appear on the hangman
+// Create an alert if user gets all letters correct, an alert will advise "You Won!", if user is out of lives, an alert will advise "Sorry, try again!"
 
-    <div class="container">
-      <p id="one">Tries Left:</p>
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="UTF-8" />
-          <link rel="stylesheet" type="text/css" href="style.css" />
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.cs"
-          />
-          <title>Hang Game</title>
-        </head>
-        <!-- 𝔾𝕦𝕖𝕤𝕤 𝕥𝕙𝕖 𝕒𝕦𝕥𝕠𝕞𝕠𝕓𝕚𝕝𝕖'𝕤 𝕓𝕣𝕒𝕟𝕕 𝕟𝕒𝕞𝕖 -->
-        <body>
-          <header>
-            <h1 id="hangman">🄷🄰🄽🄶🄼🄰🄽 🄶🄰🄼🄴</h1>
-            <p>Find your CITY, find your MATCH</p>
-          </header>
+// Array of words
+var wordArrays = [
+  "atlanta",
+  "toronto",
+  "dallas",
+  "birmingham",
+  "miami",
+  "vegas",
+  "memphis",
+  "orlando"
+];
 
-          <div class="container">
-            <p id="one">Tries Left:</p>
-            <!-- # of lives remaining -->
-            <div id="livesRemaining" class="col"></div>
+// Word that you selected
+var words;
 
-            <!-- Topics to choose from -->
-            <div id="category"></div>
+// This will randomize the words
+var randomWords = "";
 
-            <!-- This is where the underlines are -->
-            <div id="underscores" class="col"></div>
+// Deciding if to use this and use click method. Make it look like buttons
+var alphabets = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
 
-            <!-- # of wins per player -->
-            <div id="sumWins" class="col"></div>
-            <p id="two">Win Counts:</p>
+// right and wrong guesses and guesses made
+var correctGuesses;
+var choice;
+var guessesMade = [];
 
-            <!-- This is the onclick buttons -->
-            <div id="buttons" class="col"></div>
-            <!-- Start/Clear Buttons -->
-          </div>
-          <div class="container">
-            <button id="start" onclick="startGame()">Start</button>
-            <button id="clear" onclick="clearGame()">Reset</button>
-          </div>
+// Number of guesses left = 6;
+var livesRemaining = 7;
+// var livesRemaining = document.getElementById("livesRemaining");
 
-          <!-- <img src="https://cdn.instructables.com/FZN/8U7N/JCGKCTA9/FZN8U7NJCGKCTA9.LARGE.jpg" alt=""> -->
-          <!-- canvas for Hangman -->
-          <canvas id="man"></canvas>
+// Count # of wins and losses
+var sumWins = 0;
+var isInitialized = false;
+// document.getElementById("sumWins");
 
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-          <script src="hangman.js"></script>
-        </body>
-      </html>
+// document.getElementById("sumWins").innerHTML = sumWins;
 
-      <!-- # of lives remaining -->
-      <div id="livesRemaining" class="col"></div>
+// Underscores for each letter in the array
+var underscores = [];
 
-      <!-- Topics to choose from -->
-      <div id="category"></div>
+// Hangman body parts
 
-      <!-- This is where the underlines are -->
-      <div id="underscores" class="col"></div>
+// Create Alphabet buttons
+function buttonsAlpha() {
+  var newButtons = document.getElementById("buttons");
+  var lists = document.createElement("ul");
+  for (var i = 0; i < alphabets.length; i++) {
+    var btnListItems = document.createElement("li");
+    btnListItems.classList.add('container-fluid');
+    btnListItems.textContent = alphabets[i];
+    newButtons.appendChild(lists);
+    lists.appendChild(btnListItems);
+  }
+}
+buttonsAlpha();
 
-      <!-- # of wins per player -->
-      <div id="sumWins" class="col"></div>
-      <p id="two">Win Counts:</p>
+// Create underscore based on # of characters
+function randomArrays() {
+  randomWords = wordArrays[Math.floor(Math.random() * wordArrays.length)];
+  underscores = [];
+  for (var i = 0; i < randomWords.length; i++) {
+    underscores.push("_");
+  }
+  document.getElementById("underscores").innerHTML = underscores.join(" ");
+}
+randomArrays();
 
-      <!-- This is the onclick buttons -->
-      <div id="buttons" class="col"></div>
-      <!-- Start/Clear Buttons -->
-    </div>
-    <div class="container">
-      <button id="start" onclick="startGame()">Start</button>
-      <button id="clear" onclick="clearGame()">Reset</button>
-    </div>
+// Add onclick function for buttons
+document.addEventListener("click", buttonClicked);
 
-    <!-- <img src="https://cdn.instructables.com/FZN/8U7N/JCGKCTA9/FZN8U7NJCGKCTA9.LARGE.jpg" alt=""> -->
-    <!-- canvas for Hangman -->
-    <canvas id="man"></canvas>
+function buttonClicked() {
+  var clickedElement = event.target;
+  clickedElement.classList.add("buttonDisabled");
+  var chosenLetter = event.target.innerHTML;
+  console.log("var text " + chosenLetter);
+  var guessesRemaining = document.getElementById("livesRemaining").innerHTML;
+  console.log(randomWords);
+  if (guessesRemaining > 0) {
+    var letterElement = new Array();
+    for (var i = 0; i < randomWords.length; i++) {
+      if (randomWords.charAt(i) === chosenLetter.toLowerCase()) {
+        console.log("found letter " + i);
+        letterElement.push(i);
+      }
+    }
+    // console.log("letter element length " + letterElement.length);
+    if (letterElement.length < 1) {
+      guessesRemaining -= 1;
+      document.getElementById("livesRemaining").innerHTML = guessesRemaining;
+      drawParts();
+    } else {
+      for (var i = 0; i < letterElement.length; i++) {
+        underscores[letterElement[i]] = chosenLetter;
+      }
+      document.getElementById("underscores").innerHTML = underscores.join(" ");
+      if (!underscores.join().includes("_")) {
+        sumWins++;
+        alert("Congrats, you won!");
+        document.getElementById("sumWins").innerHTML = sumWins;
+      }
+    }
+  }
+}
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="hangman.js"></script>
-  </body>
-</html>
+// Function to start the game
+function startGame() {
+  randomWords = wordArrays[Math.floor(Math.random() * wordArrays.length)];
+  randomArrays();
+  document.getElementById("livesRemaining").innerHTML = livesRemaining;
+  if (!isInitialized) {
+    document.getElementById("sumWins").innerHTML = 0;
+    isInitialized = true;
+    canvas();
+  }
+}
+
+// // Function to reset the game
+function clearGame() {
+  randomWords = wordArrays[Math.floor(Math.random() * wordArrays.length)];
+  randomArrays();
+  document.getElementById("livesRemaining").innerHTML = livesRemaining;
+}
